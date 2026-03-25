e7edc575d004a61d047770be9f41983160ee8ad3
DerekVega <dereckvb9@gmail.com>
2026-03-24 22:51:38 -0600

    Update Partido.java
    Updated

--- src/main/java/paquete/modelo/Partido.java
+++ src/main/java/paquete/modelo/Partido.java
@@ -9,27 +9,61 @@ package paquete.modelo;
  * @author maria
  */
 public class Partido {
+    private String equipoLocal; 
+    private String equipoVisitante; 
+    private int golesLocales; 
+    private int golesVisitantes; 
 
-    private int idPartido;
-    private Equipo equipoLocal;
-    private Equipo equipoVisitante;
-    private String marcador;
-
-    public Partido() {
+    public Partido(String equipoLocal, String equipoVisitante) {
+        this.equipoLocal = equipoLocal;
+        this.equipoVisitante = equipoVisitante;
+        this.golesLocales = 0;
+        this.golesVisitantes = 0;
     }
 
-    public void crearPartido() {
-        System.out.println("Partido creado");
+    public String getEquipoLocal() {
+        return equipoLocal;
     }
 
-    public void registrarResultado() {
-        System.out.println("Resultado registrado: " + marcador);
+    public void setEquipoLocal(String equipoLocal) {
+        this.equipoLocal = equipoLocal;
     }
 
-    public void mostrarPartido() {
-        System.out.println("ID Partido: " + idPartido);
-        System.out.println("Equipo Local: " + equipoLocal);
-        System.out.println("Equipo Visitante: " + equipoVisitante);
-        System.out.println("Marcador: " + marcador);
+    public String getEquipoVisitante() {
+        return equipoVisitante;
     }
+
+    public void setEquipoVisitante(String equipoVisitante) {
+        this.equipoVisitante = equipoVisitante;
+    }
+
+    public int getGolesLocales() {
+        return golesLocales;
+    }
+
+    public void setGolesLocales(int golesLocales) {
+        this.golesLocales = golesLocales;
+    }
+
+    public int getGolesVisitantes() {
+        return golesVisitantes;
+    }
+
+    public void setGolesVisitantes(int golesVisitantes) {
+        this.golesVisitantes = golesVisitantes;
+    }
+    
+    public void resultado(int golesLocales, int golesVisitantes){
+        
+        this.golesLocales = golesLocales;
+        this.golesVisitantes = golesVisitantes;
+    }
+
+    @Override
+    public String toString() {
+        return equipoLocal + "VS" + equipoVisitante +
+                "Resultado"+ golesLocales + "-" + golesVisitantes;
+        
+    }
+    
 }

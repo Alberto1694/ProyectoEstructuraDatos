1c2d3cdfb944c881bae37b909357a9931f26caf5
DerekVega <dereckvb9@gmail.com>
2026-03-25 20:29:01 -0600

    Update NodoDoble.java

--- src/main/java/paquete/modelo/NodoDoble.java
+++ src/main/java/paquete/modelo/NodoDoble.java
@@ -2,12 +2,53 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package paquete.modelo;
 
 /**
  *
  * @author maria
  */
-public class NodoDoble {
+public class NodoDoble 
+{
+    private Object dato; 
+    private NodoDoble siguiente;
+    private NodoDoble anterior;
+
+    public NodoDoble(Object dato) {
+        this.dato = dato;
+        this.siguiente = null;
+        this.anterior = null;
+    }
+
+    public Object getDato() {
+        return dato;
+    }
+
+    public void setDato(Object dato) {
+        this.dato = dato;
+    }
+
+    public NodoDoble getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(NodoDoble siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    public NodoDoble getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(NodoDoble anterior) {
+        this.anterior = anterior;
+    }
+
+
+    
     
 }

3a781ee8b3aea57963a0c562893bd3a9d69f3bf5
Alberto1694 <123527485+Alberto1694@users.noreply.github.com>
2026-04-19 12:42:02 -0600

    Arbol y equipo

--- src/main/java/paquete/modelo/ArbolClasificacion.java
+++ src/main/java/paquete/modelo/ArbolClasificacion.java
@@ -9,19 +9,129 @@ package paquete.modelo;
  * @author maria
  */
 public class ArbolClasificacion {
-    private Nodo clasifica;
+    
+    private class NodoClasificacion {
+        private Equipo equipo;
+        private NodoClasificacion izquierda;
+        private NodoClasificacion derecha;
+
+        NodoClasificacion(Equipo equipo) {
+            this.equipo = equipo;
+        }
+    }
+
+    private NodoClasificacion raiz;
+
+    public ArbolClasificacion() {
+        raiz = null;
+    }
 
     public void agregar(Equipo e) {
+        if (e == null) {
+            return;
+        }
+        raiz = insertar(raiz, e);
+    }
+
+    private NodoClasificacion insertar(NodoClasificacion actual, Equipo e) {
+        if (actual == null) {
+            return new NodoClasificacion(e);
+        }
+
+        if (esMenor(e, actual.equipo)) {
+            actual.izquierda = insertar(actual.izquierda, e);
+        } else if (esMayor(e, actual.equipo)) {
+            actual.derecha = insertar(actual.derecha, e);
+        } else {
+            actual.equipo = e;
+        }
+        return actual;
     }
 
     public void actualizar(Equipo e) {
+        eliminar(e);
+        agregar(e);
     }
-    
+
     public void eliminar(Equipo e) {
+        if (e == null) {
+            return;
+        }
+        raiz = eliminar(raiz, e);
+    }
+
+    private NodoClasificacion eliminar(NodoClasificacion actual, Equipo e) {
+        if (actual == null) {
+            return null;
+        }
+
+        if (esMenor(e, actual.equipo)) {
+            actual.izquierda = eliminar(actual.izquierda, e);
+            return actual;
+        }
+
+        if (esMayor(e, actual.equipo)) {
+            actual.derecha = eliminar(actual.derecha, e);
+            return actual;
+        }
+
+        if (actual.izquierda == null) {
+            return actual.derecha;
+        }
+        if (actual.derecha == null) {
+            return actual.izquierda;
+        }
+
+        NodoClasificacion sucesor = minimo(actual.derecha);
+        actual.equipo = sucesor.equipo;
+        actual.derecha = eliminar(actual.derecha, sucesor.equipo);
+        return actual;
+    }
+
+    private NodoClasificacion minimo(NodoClasificacion nodo) {
+        NodoClasificacion actual = nodo;
+        while (actual.izquierda != null) {
+            actual = actual.izquierda;
+        }
+        return actual;
+    }
+
+    private boolean esMenor(Equipo a, Equipo b) {
+        if (a.getVictorias() < b.getVictorias()) {
+            return true;
+        }
+        if (a.getVictorias() > b.getVictorias()) {
+            return false;
+        }
+        return a.getNombreEquipo().compareToIgnoreCase(b.getNombreEquipo()) < 0;
+    }
+
+    private boolean esMayor(Equipo a, Equipo b) {
+        if (a.getVictorias() > b.getVictorias()) {
+            return true;
+        }
+        if (a.getVictorias() < b.getVictorias()) {
+            return false;
+        }
+        return a.getNombreEquipo().compareToIgnoreCase(b.getNombreEquipo()) > 0;
+    }
+
+    public String recorrerDesc() {
+        return recorrerDesc(raiz, "");
+    }
+
+    private String recorrerDesc(NodoClasificacion nodo, String texto) {
+        if (nodo == null) {
+            return texto;
+        }
+        texto = recorrerDesc(nodo.derecha, texto);
+        texto += nodo.equipo.getNombreEquipo() + " - Victorias: " + nodo.equipo.getVictorias() + "\n";
+        texto = recorrerDesc(nodo.izquierda, texto);
+        return texto;
     }
 
     public boolean estaVacia() {
-        return false;
+        return raiz == null;
     }   
   
 }
--- src/main/java/paquete/modelo/Equipo.java
+++ src/main/java/paquete/modelo/Equipo.java
@@ -14,8 +14,90 @@ public class Equipo {
     private String nombreEquipo;
     private String entrenador;
     private int cantidadJugadores;
+    private int partidosJugados;
+    private int victorias;
+    private int derrotas;
+    private int puntos;
 
     public Equipo() {
+        this(0, "", "", 0);
+    }
+
+    public Equipo(int idEquipo, String nombreEquipo, String entrenador, int cantidadJugadores) {
+        this.idEquipo = idEquipo;
+        this.nombreEquipo = nombreEquipo;
+        this.entrenador = entrenador;
+        this.cantidadJugadores = cantidadJugadores;
+        this.partidosJugados = 0;
+        this.victorias = 0;
+        this.derrotas = 0;
+        this.puntos = 0;
+    }
+
+    public int getIdEquipo() {
+        return idEquipo;
+    }
+
+    public void setIdEquipo(int idEquipo) {
+        this.idEquipo = idEquipo;
+    }
+
+    public String getNombreEquipo() {
+        return nombreEquipo;
+    }
+
+    public void setNombreEquipo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+    }
+
+    public String getEntrenador() {
+        return entrenador;
+    }
+
+    public void setEntrenador(String entrenador) {
+        this.entrenador = entrenador;
+    }
+
+    public int getCantidadJugadores() {
+        return cantidadJugadores;
+    }
+
+    public void setCantidadJugadores(int cantidadJugadores) {
+        this.cantidadJugadores = cantidadJugadores;
+    }
+
+    public int getPartidosJugados() {
+        return partidosJugados;
+    }
+
+    public int getVictorias() {
+        return victorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void sumarVictoria() {
+        partidosJugados++;
+        victorias++;
+        puntos += 3;
+    }
+
+    public void sumarDerrota() {
+        partidosJugados++;
+        derrotas++;
+    }
+
+    public void reiniciarEstadisticas() {
+        partidosJugados = 0;
+        victorias = 0;
+        derrotas = 0;
+        puntos = 0;
     }
 
     public void registrarEquipo() {
@@ -31,5 +113,10 @@ public class Equipo {
         System.out.println("Nombre Equipo: " + nombreEquipo);
         System.out.println("Entrenador: " + entrenador);
         System.out.println("Cantidad de jugadores: " + cantidadJugadores);
+    }
+
+    @Override
+    public String toString() {
+        return nombreEquipo;
     }
 }

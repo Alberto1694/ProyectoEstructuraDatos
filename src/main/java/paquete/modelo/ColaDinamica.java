8a5d363fb9d7f6fc5d95fc845d84b5d88633e2d9
DerekVega <dereckvb9@gmail.com>
2026-04-21 22:17:22 -0600

    Update ColaDinamica.java

--- src/main/java/paquete/modelo/ColaDinamica.java
+++ src/main/java/paquete/modelo/ColaDinamica.java
@@ -14,16 +14,71 @@ Funcionará bajo principio FIFO.
 El primer partido programado será el primero en jugarse.
 */
 public class ColaDinamica {
+
     private Nodo frente;
     private Nodo fin;
+    private int tamanio;
 
-    public void encolar(Partido p) {
-    }
-
-    public void desencolar() {
+    public ColaDinamica() {
+        frente = null;
+        fin = null;
+        tamanio = 0;
     }
 
     public boolean estaVacia() {
-        return false;
-    }    
+        return frente == null;
+    }
+
+    public void encolar(Object dato) {
+
+        Nodo nuevo = new Nodo(dato);
+
+        if (estaVacia()) {
+            frente = nuevo;
+            fin = nuevo;
+        } else {
+            fin.setSiguiente(nuevo);
+            fin = nuevo;
+        }
+
+        tamanio++;
+    }
+
+    public Object desencolar() {
+
+        if (estaVacia()) {
+            return null;
+        }
+
+        Object dato = frente.getDato();
+        frente = frente.getSiguiente();
+
+        if (frente == null) {
+            fin = null;
+        }
+
+        tamanio--;
+        return dato;
+    }
+
+    public Object verFrente() {
+        if (estaVacia()) {
+            return null;
+        }
+        return frente.getDato();
+    }
+
+    public int getTamanio() {
+        return tamanio;
+    }
+
+    public void imprimirCola() {
+
+        Nodo aux = frente;
+
+        while (aux != null) {
+            System.out.println(aux.getDato());
+            aux = aux.getSiguiente();
+        }
+    }
 }
